aa057cfee3b94bce3bb9d1556ca6193724300037
Saumya Dubey <saumyadb29@gmail.com>
2022-07-29 12:21:22 +0530

    Initial Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
+    screens: {},
+    colors: {},
+    fontFamily: {
+      sans: ['"Work Sans"', 'sans-serif'],
+    },
+    fontSize: {},
     extend: {},
   },
   plugins: [],
